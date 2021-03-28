6b8358062bef1f2600cf68d36f1208daabab9127
YuliyaBrui <juljadivina@gmail.com>
2021-03-28 23:26:06 +0300

    feat:add decision of transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,3 +1,38 @@
 const CustomError = require("../extensions/custom-error");
+module.exports = function transform(arr) {
+  let newArr = [];
 
-module.exports = function transform(/* arr */) {};
+  if (!Array.isArray(arr) && arr.length == 0) {
+    return Error;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == "--discard-next" && arr[i + 1] !== undefined) {
+      newArr.pop(arr[i + 1]);
+    } else if (
+      arr[i] == "--discard-prev" &&
+      arr[i - 2] !== "--discard-next" &&
+      arr[i - 1] !== undefined
+    ) {
+      newArr.pop(arr[i - 1]);
+    } else if (arr[i] == "--double-next" && arr[i + 1] !== undefined) {
+      newArr.push(arr[i + 1]);
+    } else if (
+      arr[i] == "--double-prev" &&
+      arr[i - 1] !== undefined &&
+      arr[i - 2] !== "--discard-next"
+    ) {
+      newArr.push(arr[i - 1]);
+    } else if (
+      arr[i] !== undefined &&
+      arr[i] !== "--discard-prev" &&
+      arr[i] !== "--discard-next" &&
+      arr[i] !== "--double-next" &&
+      arr[i] !== "--double-prev"
+      //arr[i - 1] !== "--discard-next" &&
+      //arr[i + 1] !== "--discard-prev"
+    ) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+};
