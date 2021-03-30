90203df5b4685d791ed087c76bb8a280eec2feec
YuliyaBrui <juljadivina@gmail.com>
2021-03-30 13:11:55 +0300

    feat:add decision of simple-chain

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,11 +1,31 @@
 const CustomError = require("../extensions/custom-error");
-
 const chainMaker = {
-  getLength() {},
-  addLink(value) {},
-  removeLink(position) {},
-  reverseChain() {},
-  finishChain() {},
+  str: [],
+  getLength() {
+    return this.str.length;
+  },
+  addLink(value) {
+    if (value == undefined) {
+      this.str.push("( " + " )");
+    } else {
+      this.str.push("( " + value + " )");
+    }
+    return this;
+  },
+  removeLink(position) {
+    if (Number.isInteger(position) == false || position > this.str.length) {
+      throw new Error();
+    }
+    this.str.splice(position - 1, 1);
+    return this;
+  },
+  reverseChain() {
+    this.str.reverse();
+    return this;
+  },
+  finishChain() {
+    return this.str.join("~~");
+  },
 };
 
 module.exports = chainMaker;
